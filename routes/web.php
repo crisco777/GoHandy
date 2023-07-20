d5eee957b7fafcb9af945d703270706ddf38d675
crisco777 <cristofer.bautista2024@poma.superate.org.sv>
2023-07-20 14:27:45 -0600

    Rutas post y get de sign up y login

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function (){
+    return view('login');
+
+});
+
+Route::post('/login', function(){
+
+}
+);
+
+Route::get('/signup', function (){
+    return view('signup');
+
+});
+
+Route::post('/signup', function(){
+
+}
+);
+

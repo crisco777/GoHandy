25986a5deda6bb26fef0d5c136063180cc041096
NetoAyala <133060710+NetoAyala@users.noreply.github.com>
2023-07-25 09:59:20 -0600

    signup

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -13,9 +13,18 @@
         <div class="signup">
             <form>
                 <label for="chk" aria-hidden="true">Sign up</label>
-                <input type="text" name="txt" placeholder="Username" required="">
-                <input type="email" name="email" placeholder="Email" required="">
-                <input type="Password" name="pswd" placeholder="Password" required="">
+                <input type="firstname" name="firstname" placeholder="Primer nombre" required="">
+                <input type="lastname" name="lastname" placeholder="Apellido" required="">
+                <input type="age" name="age" placeholder="Edad" required="">
+                <input type="contact" name="contact" placeholder="Contacto" required="">
+                <input type="sex_id" name="sex_id" placeholder="Sexo" required="">
+                <input type="age" name="age" placeholder="Edad" required="">
+                <input type="town_id" name="town_id" placeholder="Municipio" required="">
+                <input type="address" name="address" placeholder="Dirección" required="">
+                <input type="text" name="txt" placeholder="Nombre de usuario" required="">
+                <input type="email" name="email" placeholder="Correo" required="">
+                <input type="email_verified_at" name="email_verified_at" placeholder="Correo electronico verficado en" required="">
+                <input type="Password" name="pswd" placeholder="Contraseña" required="">
                 <button>Sign Up</button>
             </form>
 
@@ -24,8 +33,8 @@
         <div class="login">
             <form>
                 <label for="chk" aria-hidden="true">Login</label>
-                <input type="email" name="email" placeholder="Email" required="">
-                <input type="Password" name="pswd" placeholder="Password" required="">
+                <input type="email" name="email" placeholder="Correo" required="">
+                <input type="Password" name="pswd" placeholder="Contraseña" required="">
                 <button>Login</button>
             </form>
 

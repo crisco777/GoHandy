362356a9462a9722cf01b26bd379880e3b7df575
NetoAyala <133060710+NetoAyala@users.noreply.github.com>
2023-08-18 16:15:42 -0600

    vaya

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,10 +5,8 @@
             background: linear-gradient(to right, #4682a9, #0a2647);
         }
 
-        .bg {
-            background-image: url(img/vegeta.jpg);
-            background-position: center center;
-        }
+
+
 
         .transition {
             transition: all;
@@ -19,46 +17,47 @@
 @endpush
 
 <x-layout>
-    <div class="container w-75 bg-primary mt-5 rounded shadow" x-data="{ showSignUp: false }">
-        <div class="row align-items-stretch">
-            <div class="col bg d-none d-lg-block col-md-5 col-lg-5 col-xl-6 rounded">
-
+    <div class="container w-75 mt-3 rounded shadow" x-data="{ showSignUp: false } " >
+        <div class="row align-items-stretch" style="height: 95vh; width:150vh; background-color:white ">
+            <div class="col-6 " style="margin-top: 17vh; ">
+                <img src="img/logo.jpg" alt="" style="width:75vh">
             </div>
             <div class="col bg-white p-5 rounded-end transition" x-show="!showSignUp" x-transition:enter-start="opacity-0"
                 x-transition:enter-end="opacity-100" x-transition:leave-start="opacity-100"
                 x-transition:leave-end="opacity-0">
 
-                <h2 class="fw-bold text-center py-5">Bienvenido</h2>
+                <h2 class="fw-bold text-center mt-2 mb-4">Bienvenido</h2>
 
 
 
                 <form action="/login" method="POST">
                     @csrf
-                    <div class="mb-4">
+                    <div class="mb-5">
                         <label for="email" class="form-label">Correo electronico</label>
                         <input type="email" class="form-control" name="email">
                     </div>
-                    <div class="mb-4">
+                    <div class="mb-3">
                         <label for="password" class="form-label">Contraseña</label>
                         <input type="password" class="form-control" name="password">
                     </div>
 
                     <div class="d-grid">
-                        <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                        <button type="submit" class="btn btn-primary mt-5">Iniciar sesión</button>
                     </div>
                 </form>
 
-                <div class="my-3">
-                    <span>No tienes cuenta? <button x-on:click="showSignUp = true">Registrarme</button></span>
+                <div class="mt-3">
+                    <span>No tienes cuenta? <button class="btn btn-outline-primary" x-on:click="showSignUp = true">Registrarme</button></span>
                 </div>
             </div>
 
             <div class="col bg-white p-5 rounded-end transition" x-show="showSignUp"
+                style="height: 95vh; width:150vh"
                 x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
                 x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0">
 
 
-                <h2 class="fw-bold text-center py-3">Bienvenida</h2>
+                <h2 class="fw-bold text-center mt-2">Bienvenida</h2>
 
                 <form action="/signup" method="POST">
                     @csrf
@@ -69,7 +68,7 @@
                     <span>¿Qué quieres hacer?</span>
                     <br>
                     <br>
-                    <div class="d-flex flex-row mb-3">
+                    <div class="d-flex flex-row mb-2">
                         @foreach ($roles as $role)
                             <div class="form-check col-6">
                                 <label class="form-check-label">
@@ -86,8 +85,8 @@
                     </div>
                 </form>
 
-                <div class="my-3">
-                    <span>Ya tienes cuenta? <button x-on:click="showSignUp = false">Inicia sesion</button></span>
+                <div class="my-2">
+                    <span>Ya tienes cuenta? <button class="btn btn-outline-primary" x-on:click="showSignUp = false">Inicia sesion</button></span>
                 </div>
             </div>
         </div>

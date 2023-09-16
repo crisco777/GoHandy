06284b381673e0a2ddef50e40eff3d45106da31e
NetoAyala <133060710+NetoAyala@users.noreply.github.com>
2023-09-15 22:22:29 -0600

    Update profile.blade.php

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,60 +4,63 @@
 
     @props([])
     <div class="container wrapper mt-4 ">
-        <div class="container">
-            <div class="d-flex justify-content-center align-items-center">
-
+        <div class="row">
+            <div class="col-6">
                 <img src="{{ asset('img/todos.jpeg') }}"
                     style="width: 30vh; height:30vh; margin-top:5vh, border-radius:10vh">
             </div>
-        </div>
-        <div class="text-center row">
-            <h4 style="font-family: 'Bevan'; ">{{ $user->firstname }} {{ $user->lastname }}</h4>
-            <div class="mt-2 ">
-                <span>Email:</span>
-                <span>{{ $user->email }}</span>
-            </div>
+            <div class="col-6">
 
-            <div class="mt-2">
-                <span>Edad:</span>
-                <span>{{ $user->age }} </span>
-            </div>
+                <div>
+                    <h4 style="font-family: 'Bevan'; ">{{ $user->firstname }} {{ $user->lastname }}</h4>
+                </div>
 
-            <div class="mt-2">
-                <span>Sexo:</span>
-                <span>{{ $user->sex->type }} </span>
-            </div>
+                <div>
+                    <span class="lead">Email:</span>
+                    <span>{{ $user->email }}</span>
+                </div>
 
-            <div class="mt-2">
-                <span>Servicios disponibles:
-                    @foreach ($user->services as $service)
-                        <span>{{ $service->type }}</span>
-                    @endforeach
-                </span>
-            </div>
-            <div class="mt-2">
-                <span>Descripcion Personal</span>
-                <p>Hola jaja</p>
-            </div>
+                <div>
+                    <span>Edad:</span>
+                    <span>{{ $user->age }} </span>
+                </div>
 
-            <div class="mt-2">
-                <span>Direccion:</span>
-                <span>{{ $user->address }}</span>
+                <div>
+                    <span>Sexo:</span>
+                    <span>{{ $user->sex->type }} </span>
+                </div>
+
+                <div>
+                    <span>Servicios disponibles:
+                        @foreach ($user->services as $service)
+                            <span>{{ $service->type }}</span>
+                        @endforeach
+                    </span>
+                </div>
+
+                <div>
+                    <span>Descripcion Personal</span>
+                    <p>Hola jaja</p>
+                </div>
+
+                <div>
+                    <span>Direccion:</span>
+                    <span>{{ $user->address }}</span>
+                </div>
+
+                <div>
+                    <span>Número de contacto:</span>
+                    <p>{{ $user->contact }}</p>
+                </div>
+
             </div>
-
-            <div class="mt-2">
-                <span>Número de contacto:</span>
-                <p>{{ $user->contact }}</p>
-            </div>
-
-
             <div class="container row align-items-left">
                 <a href="{{ route('hired') }}">
-                    <button type="button" class="btn" style="background-color:#050A30; color:rgb(255, 255, 255)"> Solicitar Servicio
+                    <button type="button" class="btn" style="background-color:#050A30; color:rgb(255, 255, 255)">
+                        Solicitar Servicio
                     </button>
                 </a>
             </div>
-
         </div>
     </div>
 </x-layout>

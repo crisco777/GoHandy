a4754b4f0cc72280a1546c14dda5530135fd714a
NetoAyala <133060710+NetoAyala@users.noreply.github.com>
2023-09-17 11:47:34 -0600

    viewprofile

--- resources/views/profcomplete.blade.php
+++ resources/views/profcomplete.blade.php
@@ -1,63 +1,63 @@
 <x-layout>
 
 
-<link rel="stylesheet" href="{{ asset('css/app.css') }}">
-   </head>
-   
-  <div class="wrapper" style="width: 100vh;">
-    <span style="font-family: Bevan; font-size:30px">Registration</span>
-    <form action="/profcomplete" method="POST">
-        @csrf
-      <div class="input-box">
-        <input type="string" name="firstname" placeholder="Nombre" required>
-      </div>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
 
-      <div class="input-box">
-        <input type="string" name="lastname" placeholder="Apellido" required>
-      </div>
+    <div class="wrapper" style="width: 100vh;">
+        <span style="font-family: Bevan; font-size:30px">Registration</span>
+        <form action="/profcomplete" method="POST">
+            @csrf
+            <div class="input-box">
+                <input type="string" name="firstname" placeholder="Nombre" required>
+            </div>
 
-      <div class="input-box">
-        <input type="number" min="0" step="1" name="age" placeholder="Edad" required>
-      </div>
+            <div class="input-box">
+                <input type="string" name="lastname" placeholder="Apellido" required>
+            </div>
 
-      <div class="input-box">
-        <input type="number" name="contact" placeholder="Numero de contacto" required>
-      </div>
+            <div class="input-box">
+                <input type="number" min="0" step="1" name="age" placeholder="Edad" required>
+            </div>
 
-      <select class="form-select input-box" name="sex_id">
-        <option selected>Sexo</option>
-        @foreach ($sexes as $sex)
-            <option value="{{ $sex->id}}">{{$sex->type}}</option>
-        @endforeach
-      </select>
+            <div class="input-box">
+                <input type="number" name="contact" placeholder="Numero de contacto" required>
+            </div>
 
-      <select class="form-select input-box" name="town_id">
-        <option selected>Elije tu municipio</option>
-        @foreach ($towns as $town)
-            <option value="{{ $town->id }}">{{$town->name}}</option>
-        @endforeach
-      </select>
+            <select class="form-select input-box" name="sex_id">
+                <option selected>Sexo</option>
+                @foreach ($sexes as $sex)
+                    <option value="{{ $sex->id }}">{{ $sex->type }}</option>
+                @endforeach
+            </select>
 
-      <div class="input-box">
-        <input type="string" name="address" placeholder="Direccion" required>
-      </div>
+            <select class="form-select input-box" name="town_id">
+                <option selected>Elije tu municipio</option>
+                @foreach ($towns as $town)
+                    <option value="{{ $town->id }}">{{ $town->name }}</option>
+                @endforeach
+            </select>
 
-      @if ($userrole == 1)
-      <div class="input-box">
-          <input type="string" name="description" placeholder="Dinos un poco acerca de tí" required>
-        </div>
+            <div class="input-box">
+                <input type="string" name="address" placeholder="Direccion" required>
+            </div>
 
-        <select class="form-select input-box" name="service_id">
-            <option selected>Qué servicios te interesa ofrecer</option>
-            @foreach ($services as $service)
-                <option value="{{ $service->id }}">{{$service->type}}</option>
-            @endforeach
-          </select>
+            @if ($userrole == 1)
+                <div class="input-box">
+                    <input type="string" name="description" placeholder="Dinos un poco acerca de tí" required>
+                </div>
 
-        @endif
+                <select class="form-select input-box" name="service_id">
+                    <option selected>Qué servicios te interesa ofrecer</option>
+                    @foreach ($services as $service)
+                        <option value="{{ $service->id }}">{{ $service->type }}</option>
+                    @endforeach
+                </select>
 
-      <div class="input-box  button">
-        <input style="background-color: #050A30; color:white " type="Submit" value="Register Now">
-      </div>
-    </form>
+            @endif
+
+            <div class="input-box  button">
+                <input style="background-color: #050A30; color:white " type="Submit" value="Register Now">
+            </div>
+        </form>
 </x-layout>
--- resources/views/viewprofile.blade.php
+++ resources/views/viewprofile.blade.php
@@ -1,34 +1,52 @@
+@push('css')
+    <style>
+        body {
+            background: #4682a9;
+        }
+    </style>
+@endpush
+
+
 <x-layout>
-    <div class="container">
-        <div class="container">
-            <div class="d-flex justify-content-center align-items-center">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-6">
+                <form action="updateprofile" method="post" class="input-box">
+                    @csrf
+                    <div class="row">
+                        <div class="col 6">
+                            <p class="mb-1">Firstname</p>
+                            <input name="firstname" class="form-control" :value="$user - > firstname"
+                                value={{ Auth::user()->firstname }} />
+                            <p class="mb-1">Lastname</p>
+                            <input label="Last Name" name="lastname" class="form-control"
+                                value={{ Auth::user()->lastname }} />
 
-                <img src="{{ asset('img/logochiquito.jpg') }}" style="width: 30vh; height:30vh; margin-top:5vh">
+                        </div>
+                        <div class="col 6">
+                            <p class="mb-1">Email</p>
+                            <input label="Email" name="email" class="form-control" value={{ Auth::user()->email }} />
+                            <p class="mb-1">Age</p>
+                            <input label="Age" name="age" class="form-control mb-2"
+                                value={{ Auth::user()->age }} />
+                        </div>
+                    </div>
+                    <p class="mb-1">Sex</p>
+                    <input label="Age" name="age" class="form-control mb-2"
+                        value={{ Auth::user()->sex->type }} />
+                    <p class="mb-1">Dirección</p>
+                    <input label="Age" name="age" class="form-control mb-2" value={{ Auth::user()->address }} />
+                    <p class="mb-1">Número de contacto</p>
+                    <input label="Age" name="age" class="form-control mb-2" value={{ Auth::user()->contact }} />
+                    <div class="input-group mb-3">
+                        <label class="input-group-text" for="inputGroupFile01">Select Image</label>
+                        <input type="file" class="form-control" id="inputGroupFile01" name='img'>
+                    </div>
+                    <button type="submit" class="btn mb-4" style="background-color: #4682a9">Save Changes</button>
+                </form>
             </div>
-        </div>
-
-        <form action="updateprofile" method="post" class="">
-            @csrf
-
-            <p class="mb-1">Firstname</p>
-            <input name="firstname" class="form-control" :value="$user - > firstname" value={{ Auth::user()->firstname }} />
-            <p class="mb-1">Lastname</p>
-            <input label="Last Name" name="lastname" class="form-control" value={{ Auth::user()->lastname }} />
-            <p class="mb-1">Email</p>
-            <input label="Email" name="email" class="form-control" value={{ Auth::user()->email }} />
-            <p class="mb-1">Age</p>
-            <input label="Age" name="age" class="form-control mb-2" value={{ Auth::user()->age }} />
-            <p class="mb-1">Sex</p>
-            <input label="Age" name="age" class="form-control mb-2" value={{ Auth::user()->sex->type }} />
-            <p class="mb-1">Dirección</p>
-            <input label="Age" name="age" class="form-control mb-2" value={{ Auth::user()->address }} />
-            <p class="mb-1">Número de contacto</p>
-            <input label="Age" name="age" class="form-control mb-2" value={{ Auth::user()->contact }} />
-            <div class="input-group mb-3">
-                <label class="input-group-text" for="inputGroupFile01">Select Image</label>
-                <input type="file" class="form-control" id="inputGroupFile01" name='img'>
+            <div class="col-6">
+                <img class="rounded mx-auto d-block" src="img/criso.jpeg"
+                    style="width: 50vh; height:65vh; margin-top:5vh">
             </div>
-            <button type="submit" class="btn mb-4" style="background-color: #4682a9">Save Changes</button>
-        </form>
-    </div>
 </x-layout>

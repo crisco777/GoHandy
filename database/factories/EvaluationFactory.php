cf86e21072109691fa71c98c3084c1abe57c4297
Nellygomez503 <133053997+Nellygomez503@users.noreply.github.com>
2024-06-07 15:36:01 -0600

    Update EvaluationFactory.php

--- database/factories/EvaluationFactory.php
+++ database/factories/EvaluationFactory.php
@@ -24,6 +24,7 @@ class EvaluationFactory extends Factory
             'stars'=>fake()->numberBetween(1, 5),
             'comment'=>fake()->text(255),
             'anonym'=>fake()->boolean(),
-        ];
-    }
+        ]
+
+}
 }

8b848470d92f11c440d7767849b1b4cfcead3505
crisco777 <135669125+crisco777@users.noreply.github.com>
2024-07-17 13:51:37 +0000

    a

--- resources/views/viewprofile.blade.php
+++ resources/views/viewprofile.blade.php
@@ -14,7 +14,7 @@
 
                 <section class="userProfile card ">
                     <div class="profile ">
-                        <figure><img src="{{ asset('img/taylor.jpg') }} " alt="profile" width="230px" height="230px">
+                        <figure><img src="{{ asset('img/mariela.jpg') }} " alt="profile" width="230px" height="230px">
                         </figure>
                     </div>
                 </section>
